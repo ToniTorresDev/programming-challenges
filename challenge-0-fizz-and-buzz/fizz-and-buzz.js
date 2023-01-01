2b70b3b0a2ae4704d69750366be4cd51779fb694
Toni Torres Cuenca <ttorres@aduxia.com>
2023-01-01 11:27:01 +0100

    Clean code

--- challenge-0-fizz-and-buzz/fizz-and-buzz.js
+++ challenge-0-fizz-and-buzz/fizz-and-buzz.js
@@ -16,6 +16,5 @@ function test() {
         if (i % 3 === 0 && i % 5 === 0) console.log(i, "fizzbuzz");
         else if (i % 3 === 0) console.log(i, "fizz");
         else if (i % 5 === 0) console.log(i, "buzz");
-        
     }
 }
